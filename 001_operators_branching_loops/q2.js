0901ed21d454474f824efe3eaf015233e44d9874
srinibas <srinibaskhuntia39@gmail.com>
2022-01-10 16:09:12 +0530

    q2 done

---
+++ 001_operators_branching_loops/q2.js
@@ -0,0 +1,6 @@
+// Write a program to take a number input from user and determine whether the number is odd or even.
+
+let oddEven=num=>num%2===0?"even":"odd";
+
+console.log(oddEven(21));
+console.log(oddEven(42));
